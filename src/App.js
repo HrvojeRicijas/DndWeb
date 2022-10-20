366df48928e6b3dac6d9daf23fba7a0fbb1efe30
Mark Turkovic <mark.turkovic@transcom.com>
2022-10-20 13:13:03 +0200

    Mark - Swith viewer button

--- src/App.js
+++ src/App.js
@@ -1,114 +1,11 @@
-import BasicCard from "./Components/BasicCard";
-import BasicList from "./Components/BasicList";
-import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
+import React from "react";
 import Paper from "@mui/material/Card";
-import logo1 from "./Photos/637880558264506587.jpeg";
+import TestRender from "./Components/TestRender";
 
 function App() {
-  let render = 1;
-  let page = "";
-
-  function ViewMod() {
-    console.log("Bok");
-    if (render === 1) render = 0;
-    else if (render === 0) render = 1;
-    console.log("Borj je:", render);
-  }
-
-  if (render === 0) {
-    page = (
-      <BasicList name="nix" race="Changeling" class="Warlock" auth="Mark" />
-    );
-  } else if (render === 1) {
-    page = (
-      <div>
-        <BasicCard
-          name="nix"
-          race="Changeling"
-          class="Warlock"
-          auth="Mark"
-          logo={logo1}
-        />
-        <BasicCard
-          name="nix"
-          race="Changeling"
-          class="Warlock"
-          auth="Mark"
-          logo={logo1}
-        />
-        <BasicCard
-          name="nix"
-          race="Changeling"
-          class="Warlock"
-          auth="Mark"
-          logo={logo1}
-        />
-        <BasicCard
-          name="nix"
-          race="Changeling"
-          class="Warlock"
-          auth="Mark"
-          logo={logo1}
-        />
-        <BasicCard
-          name="nix"
-          race="Changeling"
-          class="Warlock"
-          auth="Mark"
-          logo={logo1}
-        />
-        <BasicCard
-          name="nix"
-          race="Changeling"
-          class="Warlock"
-          auth="Mark"
-          logo={logo1}
-        />
-        <BasicCard
-          name="nix"
-          race="Changeling"
-          class="Warlock"
-          auth="Mark"
-          logo={logo1}
-        />
-        <BasicCard
-          name="nix"
-          race="Changeling"
-          class="Warlock"
-          auth="Mark"
-          logo={logo1}
-        />
-        <BasicCard
-          name="nix"
-          race="Changeling"
-          class="Warlock"
-          auth="Mark"
-          logo={logo1}
-        />
-        <BasicCard
-          name="nix"
-          race="Changeling"
-          class="Warlock"
-          auth="Mark"
-          logo={logo1}
-        />
-      </div>
-    );
-  }
-
   return (
     <Paper>
-      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid item xs={10}>
-          {page}
-        </Grid>
-        <Grid item xs={2}>
-          <Button sx={{ marginRight: 3, marginTop: 1 }} variant="contained" onClick={ViewMod}>
-            Click me
-          </Button>
-        </Grid>
-      </Grid>
+      <TestRender />
     </Paper>
   );
 }
---
+++ src/Components/TestRender.js
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+import BasicCard from "./BasicCard";
+import BasicList from "./BasicList";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import logo1 from "../Photos/637880558264506587.jpeg";
+
+function TestRender(props) {
+  const [button, setButton] = useState(1);
+  const [view, setView] = useState(
+    <BasicList name="nix" race="Changeling" class="Warlock" auth="Mark" />
+  );
+  function viewHandler() {
+    if (button === 0) {
+      console.log("button je bio 0");
+      setButton(1);
+      setView(
+        <BasicList name="nix" race="Changeling" class="Warlock" auth="Mark" />
+      );
+    } else if (button === 1) {
+      console.log("button je bio 1");
+      setButton(0);
+      setView(
+        <div>
+          <BasicCard
+            name="nix"
+            race="Changeling"
+            class="Warlock"
+            auth="Mark"
+            logo={logo1}
+          />
+          <BasicCard
+            name="nix"
+            race="Changeling"
+            class="Warlock"
+            auth="Mark"
+            logo={logo1}
+          />
+          <BasicCard
+            name="nix"
+            race="Changeling"
+            class="Warlock"
+            auth="Mark"
+            logo={logo1}
+          />
+          <BasicCard
+            name="nix"
+            race="Changeling"
+            class="Warlock"
+            auth="Mark"
+            logo={logo1}
+          />
+          <BasicCard
+            name="nix"
+            race="Changeling"
+            class="Warlock"
+            auth="Mark"
+            logo={logo1}
+          />
+          <BasicCard
+            name="nix"
+            race="Changeling"
+            class="Warlock"
+            auth="Mark"
+            logo={logo1}
+          />
+          <BasicCard
+            name="nix"
+            race="Changeling"
+            class="Warlock"
+            auth="Mark"
+            logo={logo1}
+          />
+          <BasicCard
+            name="nix"
+            race="Changeling"
+            class="Warlock"
+            auth="Mark"
+            logo={logo1}
+          />
+          <BasicCard
+            name="nix"
+            race="Changeling"
+            class="Warlock"
+            auth="Mark"
+            logo={logo1}
+          />
+          <BasicCard
+            name="nix"
+            race="Changeling"
+            class="Warlock"
+            auth="Mark"
+            logo={logo1}
+          />
+        </div>
+      );
+    }
+    console.log({ button });
+  }
+
+  return (
+    <div>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={10}>
+          {view}
+        </Grid>
+        <Grid item xs={2}>
+          <Button
+            sx={{ marginRight: 3, marginTop: 1 }}
+            variant="contained"
+            onClick={viewHandler}
+          >
+            Click me
+          </Button>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default TestRender;
+
+/*
+
+
+*/
