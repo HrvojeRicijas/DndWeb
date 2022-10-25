7527160186de3bdc01b23f4c41b9669e8ce725b1
hrvoje <hrvoje.ricijas@gmail.com>
2022-10-25 16:59:13 +0200

    rici - DB setup

---
+++ src/server/dbSetup.sql
@@ -0,0 +1,85 @@
+CREATE DATABASE IF NOT EXISTS dndweb;
+
+DROP TABLE IF EXISTS characters;
+DROP TABLE IF EXISTS campaigns;
+DROP TABLE IF EXISTS classes;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE classes(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	class VARCHAR(55)
+);
+
+
+
+INSERT INTO classes(class) VALUES 
+	('Artificer'),
+	('Barbarian'),
+	('Bard'),
+	('Cleric'),
+	('Druid'),
+	('Fighter'),
+	('Monk'),
+	('Paladin'),
+	('Ranger'),
+	('Rogue'),
+	('Sorcerer'),
+	('Warlock'),
+	('Wizard');
+
+
+CREATE TABLE users(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	username VARCHAR(255)
+);
+
+INSERT INTO users(username) VALUES 
+	('Marko'),
+	('Laura'),
+	('Peter'),
+	('Pero'),
+	('Branimir'),
+	('Drvoje'),
+	('Briga'),
+	('Luce'),
+	('Karnelija'),
+	('Alan'),
+	('Dodo'),
+	('Vrabac'),
+	('Coca cola');
+	
+CREATE TABLE campaigns(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	NAME VARCHAR(255),
+	creator INT,
+	FOREIGN KEY (creator) REFERENCES users(id) 
+);
+
+INSERT INTO campaigns(NAME, creator) VALUES 
+	('Valem', 1),
+	('Realm', 5),
+	('Menethil', 5),
+	('Totally not Menethil', 6),
+	('Alrune', 5);
+
+CREATE TABLE characters(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	NAME VARCHAR(255),
+	race VARCHAR(255),
+	classId INT,
+	LEVEL INT,	
+	creatorId INT,
+	campaignId INT,
+	DESCRIPTION TEXT,
+	image VARCHAR(255),
+	FOREIGN KEY (classId) REFERENCES classes(id),
+	FOREIGN KEY (creatorId) REFERENCES users(id),
+	FOREIGN KEY (campaignId) REFERENCES campaigns(id)
+);
+
+INSERT INTO characters(NAME, race, classId, LEVEL, creatorId, campaignId, DESCRIPTION) VALUES 
+	('Milo Highhill', 'Shaved Goblin', 9, 10, 1, 5, "Similar to his player, a small goblinoid mapmaker"),
+	('The Protector', 'Beef Jerky', 6, 10, 3, 5, "Big simp"),
+	('Zaya', 'Fire Elemental obviously', 12, 2, 5, 2, "Fireball goes brr"),
+	('Halasir', 'Tashas optimized charisma', 12, 10, 6, 5, "Small simp"),
+	('Alitana', 'Elf minus ears minus leg', 10, 10, 2, 5, "Enjoys long walks on the beach and murdering unicorns");
--- src/views/landing.js
+++ src/views/landing.js
@@ -5,12 +5,6 @@ function Landing(props) {
     
     return (
         <div>
-            
-            OK cisto da vidis, stavljam na main page, budes ga nasao xD
-            Dakle ideja je da componenti ne idu direktno u app nego od njih slazes vieowe/screenove, ti screenovi idu u views folder i onda tek u app, to je nekakav standard za MVC applikacije. Ako tebi tutorial za react kaze nesto drugo da se radi sa reactom budemo tako ali ces mi onda morati to objesniti.
-            Baza je kinda postavljena, jos ne radi nis previse korisno, morat ces to preko xamppa srediti, to se sve lako napravi, na cu napraviti sql script koj to sredi sve, budemo na kavi sve dogovorili.
-            <br/>
-            Kada radis front, probaj resizeati screen, stvari se squishaju, to treba prek cssa pofixati
             <TestRender/>
             <CreateTest/>
         </div>
