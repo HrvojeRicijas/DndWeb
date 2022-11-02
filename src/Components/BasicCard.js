7ea4225d349c213c41f1a2d3a91d220ed607609d
Mark Turkovic <mark.turkovic@transcom.com>
2022-11-02 18:33:45 +0100

    Mark - Dynamic Card and Card text

--- src/Components/BasicCard.js
+++ src/Components/BasicCard.js
@@ -1,103 +1,112 @@
 import Card from "@mui/joy/Card";
-import ButtonBase from "@mui/material/ButtonBase";
 import CardCover from "@mui/joy/CardCover";
 import Typography from "@mui/joy/Typography";
 import CardContent from "@mui/joy/CardContent";
 import Grid from "@mui/material/Grid";
 import "../css/app.css";
-
+import React, { useState, useEffect } from "react";
 
 function BasicCard(props) {
-  let logoDef = require(`../../public/Photos/${props.logo}`)
+  let width = window.innerWidth;
+  const [ratio, setRatio] = useState(width / 1920);
+
+  useEffect(() => {
+    function handleWindowResize() {
+      width = window.innerWidth;
+      setRatio(width / 1920);
+    }
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  let logoDef = require(`../../public/Photos/${props.logo}`);
   return (
-    <ButtonBase sx={{width: "100%"}}>
-      <Card raised="ture" sx={{ height: "100%", width: "100%" }}>
-        <CardCover>
-          <img
-            src={logoDef}
-            srcSet={logoDef}
-            loading="lazy"
-            alt="Character Portrait"
-          />
-        </CardCover>
-        <CardCover
-          sx={{
-            background: `linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0,0,0,0) 250px)`,
-          }}
+    <Card raised="ture" sx={{ width: "100%", paddingTop: "75%" }}>
+      <CardCover>
+        <img
+          src={logoDef}
+          srcSet={logoDef}
+          loading="lazy"
+          alt="Character Portrait"
         />
-        <CardContent sx={{ justifyContent: "flex-end" }}>
-          <Grid
-            container
-            rowSpacing={1}
-            columnSpacing={{ xs: 1, sm: 2, md: 1 }}
-          >
-            <Grid item xs={12}>
-              <Typography
-                sx={{
-                  fontWeight: "bold",
-                  fontSize: 20,
-                  textTransform: "capitalize",
-                  border: 1,
-                  borderRadius: "10px",
-                  borderColor: "grey.500",
-                }}
-                textColor="#fff"
-                mb={1}
-              >
-                {props.name}
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography
-                flexDirection="row"
-                textColor="#f2f2f2"
-                sx={{
-                  textTransform: "capitalize",
-                  fontWeight: "regular",
-                  fontSize: 12,
-                  border: 1,
-                  borderRadius: "10px",
-                  borderColor: "grey.500",
-                }}
-              >
-                {props.race}
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography
-                flexDirection="row"
-                textColor="#f2f2f2"
-                sx={{
-                  textTransform: "capitalize",
-                  fontWeight: "regular",
-                  fontSize: 12,
-                  border: 1,
-                  borderRadius: "10px",
-                }}
-              >
-                {props.class}
-              </Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <Typography
-                flexDirection="row"
-                textColor="red"
-                sx={{
-                  textTransform: "capitalize",
-                  fontWeight: "light",
-                  fontSize: 12,
-                  fontStyle: "italic",
-                  border: 1,
-                  borderRadius: "7px",
-                }}
-              >
-                Author: {props.auth}
-              </Typography>
-            </Grid>
+      </CardCover>
+      <CardCover
+        sx={{
+          background: `linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0,0,0,0) 250px)`,
+        }}
+      />
+      <CardContent sx={{ justifyContent: "flex-end" }}>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 1 }}>
+          <Grid item xs={12}>
+            <Typography
+              sx={{
+                fontWeight: "bold",
+                fontSize: 30 * ratio,
+                textTransform: "capitalize",
+                border: 1,
+                borderRadius: "10px",
+                borderColor: "grey.500",
+              }}
+              textColor="#fff"
+              mb={1}
+            >
+              {props.name}
+            </Typography>
           </Grid>
-        </CardContent>
-      </Card>
-    </ButtonBase>
+          <Grid item xs={6}>
+            <Typography
+              flexDirection="row"
+              textColor="#f2f2f2"
+              sx={{
+                textTransform: "capitalize",
+                fontWeight: "regular",
+                fontSize: 18 * ratio,
+                border: 1,
+                borderRadius: "10px",
+                borderColor: "grey.500",
+              }}
+            >
+              {props.race}
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography
+              flexDirection="row"
+              textColor="#f2f2f2"
+              sx={{
+                textTransform: "capitalize",
+                fontWeight: "regular",
+                fontSize: 18 * ratio,
+                border: 1,
+                borderRadius: "10px",
+              }}
+            >
+              {props.class}
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography
+              flexDirection="row"
+              textColor="red"
+              sx={{
+                textTransform: "capitalize",
+                fontWeight: "light",
+                fontSize: 18 * ratio,
+                fontStyle: "italic",
+                border: 1,
+                borderRadius: "7px",
+              }}
+            >
+              Author: {props.auth}
+            </Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
   );
 }
 
--- src/views/LandingView.js
+++ src/views/LandingView.js
@@ -1,19 +1,41 @@
+import React, { useState, useEffect } from "react";
 import BasicCard from "../Components/BasicCard";
-import testjson from "../tetsdata/campaigns.json";
 import Grid from "@mui/material/Grid";
+import ButtonBase from "@mui/material/ButtonBase";
+import Axios from "axios";
 
 //import logo1 from "../Photos/Nix.jpeg";
 
 function Landing() {
+  const [campaignList, setCampaignList] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3002/api/campaigns/get").then((data) => {
+      console.log(data);
+      setCampaignList(data.data);
+    });
+  }, []);
+
   return (
-    <Grid container columnSpacing={1} rowSpacing={1} sx={{mx: 1, my: 1, gridAutoRows: "100px"}} >
-      {testjson.map((test, key) => (
-        <Grid item xs={3} key={key}> 
-          <BasicCard name={test.name} auth={test.DM} logo={test.logo} />
+    <Grid
+      container
+      columnSpacing={1}
+      rowSpacing={1}
+      sx={{ mx: 1, my: 1, gridAutoRows: "100px" }}
+    >
+      {campaignList.map((data, key) => (
+        <Grid item xs={3} key={key}>
+          <ButtonBase sx={{ width: "100%" }}>
+            <BasicCard
+              logo="Nix.jpeg"
+              name={data.camapignName}
+              auth={data.creatorName}
+            />
+          </ButtonBase>
         </Grid>
       ))}
     </Grid>
   );
 }
 
-export default Landing 
+export default Landing;
